47d77431e73e444fd1152459969bbfe31aee947c
Ravi Ranjan <ravi848101mnb@gmail.com>
2023-05-09 16:36:22 +0530

    services__

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Contact from "./components/Contact";
+import Services from "./components/Services";
 import "./styles/App.scss";
 import "./styles/Header.scss";
 import "./styles/Home.scss";
@@ -19,6 +20,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/contact" element={<Contact></Contact>}></Route>
+        <Route path="/services" element={<Services></Services>}></Route>
       </Routes>
       <Footer></Footer>
     </Router>
---
+++ src/components/Services.jsx
@@ -0,0 +1,9 @@
+
+
+const Services = () => {
+    return (
+        <div><h1>Baad me bnata hu bhut ho gya aaaj k liye avi busy hu</h1></div>
+    )
+}
+
+export default Services
