d3ec37f0997a8c846d82abef867d409dacbe58ee
Ravi Ranjan <ravi848101mnb@gmail.com>
2023-05-09 15:11:49 +0530

    contact_us_page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,8 +3,31 @@
 const Contact = () => {
     return (
         <div className="contact">
-            <main><h1>Badd me bnata hu avi busy hu
-            </h1></main>
+            <main>
+                <h1>Contact us</h1>
+
+
+                <form>
+                    <div>
+                        <label >Name</label>
+                        <input type="text" required placeholder="Abc" />
+                    </div>
+
+                    <div>
+                        <label >Email</label>
+                        <input type="email" required placeholder="Abc@gmail.com" />
+                    </div>
+
+
+                    <div>
+                        <label >Message</label>
+                        <input type="text" required placeholder="Enter your message here..." />
+                    </div>
+
+                    <button type="submit">send</button>
+                </form>
+
+            </main>
         </div>
     )
 }
