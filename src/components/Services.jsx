c60122357291fec463057ec4d598e734bc15371f
Ravi Ranjan <ravi848101mnb@gmail.com>
2023-05-10 13:21:54 +0530

    .

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -3,8 +3,7 @@
 const Services = () => {
     return (
         <div><h1>Baad me bnata hu </h1>
-            <h1>avi Arnav ka immersion me ho gya</h1>
-            <h1>to party le ka aata hu.....😗😗😗😗😗</h1>
+
         </div>
 
     )
